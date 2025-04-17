da417f464b0be3095c44db56ccc2c1b61d8ff4a0
radchu-ragasu <radsiga.20222082@iit.ac.lk>
2025-04-17 16:00:32 +0530

    basic counter App finnished

--- function.js
+++ function.js
@@ -1,14 +1,20 @@
+let count = 0;
+
+
 function Add()
 {
-
+    count++;
+    document.getElementById('number').innerText = count;
 }
 
 function Subtract()
 {
-
+    count--;
+    document.getElementById('number').innerText = count;
 }
 
 function Reset()
 {
-
+    count = 0;
+    document.getElementById('number').innerText = count;
 }
