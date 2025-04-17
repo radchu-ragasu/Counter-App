2cde725d99b494f2935f8cb187970e7abd2025af
radchu-ragasu <radsiga.20222082@iit.ac.lk>
2025-04-17 16:22:35 +0530

    odd even number check

--- function.js
+++ function.js
@@ -5,16 +5,42 @@ function Add()
 {
     count++;
     document.getElementById('number').innerText = count;
+    OddEvenCheck(count);
 }
 
 function Subtract()
 {
-    count--;
-    document.getElementById('number').innerText = count;
+    if (count == 0)
+    {
+        document.getElementById('number').innerText = count;
+        alert("Can't go below 0");
+    }
+    else{
+        count--;
+        document.getElementById('number').innerText = count;
+    }
+    
 }
 
 function Reset()
 {
     count = 0;
     document.getElementById('number').innerText = count;
+}
+
+
+function OddEvenCheck(count)
+{
+    if (count == 0)
+    {
+        document.getElementById('OddEven').innerText = "This is Zero";
+    }
+    else if (count % 2 ==0)
+    {
+        document.getElementById('OddEven').innerText= "This is Even Number";
+    }
+    else if(count % 2 == 1)
+    {
+        document.getElementById('OddEven').innerText = "This is Odd Number";
+    }
 }
